882551f1a38a6e921ca4e8509336d8dc5658e8e0
Andrei ILchenko <andrei.ilchenko@ing.nl>
2017-02-09 14:01:15 +0100

    Simple solution without parallelism

---
+++ Problem18/src/main/scala/projecteuler/Problem18.scala
@@ -0,0 +1,76 @@
+package projecteuler
+
+
+import scala.collection._
+
+/**
+  * Created by ilchen on 09/02/2017.
+  */
+
+sealed case class Node(v: Int, left: Node, right: Node) {
+  def hasChildren(): Boolean = left != null
+  override def hashCode(): Int = super.hashCode()
+}
+
+class Triangle(val root: Node) {
+
+  def findMaxPathSumNaive(): Int = findMaxPathSumHelper(root)
+  private def findMaxPathSumHelper(subtree: Node): Int = {
+    if (!subtree.hasChildren())  subtree.v
+    else  Math.max(findMaxPathSumHelper(subtree.left), findMaxPathSumHelper(subtree.right)) + subtree.v
+  }
+
+
+  def findMaxPathSum(): Int = findMaxPathSumHelper(root, mutable.Map.empty)
+  private def findMaxPathSumHelper(subtree: Node, accu: mutable.Map[Node, Int]): Int = {
+    if (!subtree.hasChildren())       subtree.v
+    else if (accu.contains(subtree))  accu(subtree)
+    else  {
+      val leftSum = findMaxPathSumHelper(subtree.left, accu)
+      val rightSum = findMaxPathSumHelper(subtree.right, accu)
+      val sum = Math.max(leftSum, rightSum) + subtree.v
+      accu += (subtree -> sum)
+      sum
+    }
+  }
+
+}
+object Triangle {
+
+  def apply(nodes: Array[Int]): Triangle = {
+    val numLines = (Math.sqrt(nodes.length * 8 + 1) - 1) / 2
+    if (numLines != Math.floor(numLines))
+      throw new IllegalArgumentException(s"Cannot make a triangle out of ${nodes.length} elements")
+    val n: Int = numLines.asInstanceOf[Int]
+    var (rest, lastRow) = nodes.splitAt(nodes.length - n)
+    var accu = lastRow.map(Node(_, null, null))
+    for (i <- n-1 to 1 by -1) {
+      val (r, l) = rest.splitAt(rest.length - i)
+      rest = r
+      lastRow = l
+      val newRow = (0 until i).map(idx => Node(lastRow(idx), accu(idx), accu(idx + 1)))
+      accu = newRow.foldRight(accu)((node, a) => node +: a)
+
+    }
+    println(s"Tree of height $n constructed")
+    new Triangle(accu.head)
+  }
+
+  /** Factory method for out of a comma-separated list (no line breaks) */
+  def apply(str: String): Triangle = {
+    apply(str.split("\\s*,\\s*").map(_.toInt))
+  }
+
+  /** Factory method for out of a whitespace-separated list (with possible line breaks) */
+  def apply(lines: Array[String]): Triangle = {
+    apply(lines.flatMap(_.split("\\s+")).map(_.toInt))
+  }
+
+  /** Factory method to make a Triangle with a required number of rows out of random numbers */
+  def apply(numRows: Int): Triangle = {
+    val maxVal = 100
+    val r = new scala.util.Random
+    apply((0 until (numRows * numRows + numRows) / 2).toArray map (_ => r.nextInt(maxVal)))
+  }
+}
+
---
+++ Problem18/src/test/scala/projecteuler/Problem18Suite.scala
@@ -0,0 +1,48 @@
+package projecteuler
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+import scala.io.Source
+
+
+
+@RunWith(classOf[JUnitRunner])
+class Problem18Suite extends FunSuite {
+  val prob18Triangle = "75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47, 65, 19, 01, 23," +
+    "75, 03, 34, 88, 02, 77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70," +
+    "33, 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28," +
+    "77, 73, 17, 78, 39, 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 04, 68, 89," +
+    "53, 67, 30, 73, 16, 69, 87, 40, 31, 04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23";
+
+  test("Problem18 works with 15 rows, naive") {
+    val triangle = Triangle(prob18Triangle)
+    val res = triangle.findMaxPathSumNaive()
+    assert(res == 1074)
+  }
+
+  test("Problem18 works with 15 rows") {
+    val triangle = Triangle(prob18Triangle)
+    val res = triangle.findMaxPathSum()
+    assert(res == 1074)
+  }
+
+  test("Problem18 works with 4 rows") {
+    val triangle = Triangle("3, 7, 4, 2, 4, 6, 8, 5, 9, 3")
+    val res = triangle.findMaxPathSum()
+    assert(res == 23)
+  }
+
+  test("Problem67 works") {
+    val triangle = Triangle(Source.fromFile(
+      "C:\\work\\pe\\Problem18\\tests\\projecteuler\\p067_triangle.txt").getLines().toArray)
+    val res = triangle.findMaxPathSum()
+    println(res)
+    assert(res == 7273)
+  }
+}
+
+
+
+  
