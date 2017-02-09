c2c32ac1e50db2f1abdb00aaec95e3c658144190
Andrei ILchenko <andrei.ilchenko@ing.nl>
2017-02-09 14:38:47 +0100

    Degenerate cases

--- Problem18/src/test/scala/projecteuler/Problem18Suite.scala
+++ Problem18/src/test/scala/projecteuler/Problem18Suite.scala
@@ -42,7 +42,7 @@ class Problem18Suite extends FunSuite {
     assert(res == 7273)
   }
 
-  test("Problem18 works with degenerate cases") {
+  test("Problem18 works with for degenerate cases") {
     val (res1, res2) = (Triangle("1") findMaxPathSum, Triangle("1, 2, 3") findMaxPathSum)
     assert(res1 == 1  &&  res2 == 4)
     try {
@@ -50,7 +50,7 @@ class Problem18Suite extends FunSuite {
       fail()
     } catch  {
       case e: IllegalArgumentException => // all well
-      case e: Exception => fail()
+      case _ => fail()
     }
   }
 }
